494d2d4ff0acf857dac30adfb218b877a50f8923
sourav <souravkrishna358@gmail.com>
2024-08-25 21:16:28 +0530

    initial commit

---
+++ Array/reversearray.js
@@ -0,0 +1,12 @@
+ let array=new Array(10);
+ array=[1,2,3,4,5,6,7,8,9,10];
+
+let reversed_array=array.reverse();
+for(let num of reversed_array){
+console.log(num);
+}
+
+
+for(i=9;i>=0;i--){
+   console.log(array[i]);
+}
---
+++ Pattern/first.js
@@ -0,0 +1,23 @@
+ /*  *  *  *  *  * 
+     *  *  *  *  * 
+     *  *  *  *  * 
+     *  *  *  *  * 
+     *  *  *  *  *       */
+
+let k;
+for(let i = 0; i < 5; i++){
+     k=Array(5).fill(" * ").join('') 
+ console.log(k);
+}
+
+
+
+
+let num;
+for(let i=0;i<5;i++){
+    num=''
+    for(let j=0;j<5;j++){
+    num +='* '
+    }
+    console.log(num);
+}
